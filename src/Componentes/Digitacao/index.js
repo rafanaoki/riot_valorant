d389165f5858f7d6f1642c5fb675f17ae84d1d96
Rafael Naoki Oka <rafael.oka152@gmail.com>
2023-02-13 21:49:08 -0300

    removendo react-typing

--- src/Componentes/Digitacao/index.js
+++ src/Componentes/Digitacao/index.js
@@ -1,15 +1,8 @@
-import Typed from "react-typed";
 import './Digitacao.css'
+
 const Digitacao = (props) => {
     return (
-        <Typed className="digitacao"
-      strings={[
-            `${props.texto}`
-          ]}
-          typeSpeed={100}
-          backSpeed={100}
-          loop
-        />
+      <p>{props.texto}</p>
     )
 }
 
